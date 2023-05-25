878068a512b05a877eb8e38bf189cbb06490b9c9
ZouhairSpace <jeddi.zouhair@gmail.com>
2023-05-25 14:48:05 -0700

    _myexit, _mycd, and _myhelp functions

---
+++ built.c
@@ -0,0 +1,100 @@
+#include "main.h"
+
+/**
+ * _myexit - exits the shell
+ * @info: structure containing potential arguments
+ *
+ * Return: exits with a given exit status (0) if info->argv[0] != "exit"
+ */
+int _myexit(info_t *info)
+{
+	int exit_check;
+
+	if (info->argv[1]) /* If there is an exit argument */
+	{
+		exit_check = _erratoi(info->argv[1]);
+		if (exit_check == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+
+/**
+ * _mycd - changes the current directory of the process
+ * @info: structure containing potential arguments
+ *
+ * Return: Always 0
+ */
+int _mycd(info_t *info)
+{
+	char *current_dir, *dir, buffer[1024];
+	int chdir_ret;
+
+	current_dir = getcwd(buffer, 1024);
+	if (!current_dir)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret = /* TODO: what should this be? */
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(current_dir);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD="));
+		_putchar('\n');
+		chdir_ret = /* TODO: what should this be? */
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]);
+		_eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * _myhelp - displays help information
+ * @info: structure containing potential arguments
+ *
+ * Return: Always 0
+ */
+int _myhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
+	return (0);
+}
+
+
