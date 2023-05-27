3eabe604a077035dbd8f4263bad378ab618465b2
ZouhairSpace <jeddi.zouhair@gmail.com>
2023-05-26 17:52:00 -0700

    Add string utility functions

---
+++ str0.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @str: the string whose length to check
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *str)
+{
+	int length = 0;
+
+	if (!str)
+		return (0);
+
+	while (*str++)
+		length++;
+	return (length);
+}
+
+/**
+ * _strcmp - performs lexicographic comparison of two strings.
+ * @str1: the first string
+ * @str2: the second string
+ *
+ * Return: negative if str1 < str2, positive if str1 > str2,
+ * zero if str1 == str2
+ */
+int _strcmp(char *str1, char *str2)
+{
+	while (*str1 && *str2)
+	{
+		if (*str1 != *str2)
+			return (*str1 - *str2);
+		str1++;
+		str2++;
+	}
+	if (*str1 == *str2)
+		return (0);
+	else
+		return (*str1 < *str2 ? -1 : 1);
+}
+
+/**
+ * starts_with - checks if needle starts with haystack
+ * @haystack: string to search
+ * @needle: the substring to find
+ *
+ * Return: address of next character of haystack or NULL
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: the destination buffer
+ * @src: the source buffer
+ *
+ * Return: pointer to destination buffer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *result = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (result);
+}
+
